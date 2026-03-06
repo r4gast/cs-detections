24380a16d12100f8c51ae9a3dfabdeb2e38f9dec
Kaan <59264452+r4gast@users.noreply.github.com>
2026-03-06 11:03:54 +0100

    Update yaml-validator.py

--- scripts/yaml-validator.py
+++ scripts/yaml-validator.py
@@ -16,7 +16,7 @@ def validate_file(file_path):
   with open(file_path, "r") as f:
     try:
       data = yaml.safe_load(f)
-      print(data)
+      print(data["name"])
     except yaml.YAMLError as e:
       print(f"YAML syntax error in {file_path}")
       print(e)
